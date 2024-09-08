70c13cca96478b2c4fc13f52a4059cef0886e3e1
CARD1NAL <cardinal312@github.com>
2024-09-08 18:17:23 +0600

    JUST TESTED WEB SOCKETS WORKS WITHOUT ANY FRAMEWORKS

--- WebSockets/ViewController.swift
+++ WebSockets/ViewController.swift
@@ -7,13 +7,77 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController, URLSessionWebSocketDelegate {
+    
+    private var webSocket: URLSessionWebSocketTask?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+        title = "Web Socket Test"
+        view.backgroundColor = .yellow
+        
+        let session = URLSession(configuration: .default, delegate: self, delegateQueue: OperationQueue())
+        guard let url = URL(string: "wss://echo.websocket.org") else { return }
+        webSocket = session.webSocketTask(with: url)
+        webSocket?.resume()
+        
+    }
+    
+    private func ping() {
+        webSocket?.sendPing { error in
+            if let error = error {
+                print("DEBUG: - PING ERROR --> \(error)")
+            }
+        }
+    }
+    
+    private func close() {
+        webSocket?.cancel(with: .goingAway, reason: "Demo ended".data(using: .utf8))
+    }
+    
+    private func send() {
+        DispatchQueue.global().asyncAfter(deadline: .now() + 2) { [weak self] in
+            self?.send()
+            self?.webSocket?.send(.string("Send new message - \(Int.random(in: 0..<999))"), completionHandler: { error in
+                if let error = error {
+                    print("Send error - \(error)")
+                }
+            })
+        }
+    }
+    
+    private func receive() {
+        webSocket?.receive { [weak self] results in
+            guard let self else { return }
+            switch results {
+            case .success(let message):
+                switch message {
+                case .data(let data):
+                    print("Data - \(data)")
+                case .string(let string):
+                    print("Got string - \(string)")
+                @unknown default:
+                    break
+                }
+            case .failure(let error):
+                print("Receive error - \(error)")
+            }
+            receive()
+        }
     }
 
+    // MARK: - WEB SOCKET METHODS
+    func urlSession(_ session: URLSession, webSocketTask: URLSessionWebSocketTask, didOpenWithProtocol protocol: String?) {
+        print("Did connect to socket")
+        self.ping()
+        self.receive()
+        self.send()
+    }
+    
+    func urlSession(_ session: URLSession, webSocketTask: URLSessionWebSocketTask, didCloseWith closeCode: URLSessionWebSocketTask.CloseCode, reason: Data?) {
+        print("Did close connection with reason")
+    }
 
 }
 
