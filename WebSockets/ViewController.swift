378e069a1cfa971bfb81f591b524d1f657499ba5
CARD1NAL <cardinal312@github.com>
2024-09-08 18:46:06 +0600

    Added close button for unconnection

--- WebSockets/ViewController.swift
+++ WebSockets/ViewController.swift
@@ -10,12 +10,26 @@ import UIKit
 final class ViewController: UIViewController, URLSessionWebSocketDelegate {
     
     private var webSocket: URLSessionWebSocketTask?
+    private let closeButton: UIButton = {
+        $0.backgroundColor = .white
+        $0.setTitle("Close socket", for: .normal)
+        $0.setTitleColor(.black, for: .normal)
+        $0.layer.cornerRadius = 12
+        $0.layer.borderWidth = 0.5
+        $0.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        $0.frame = CGRect(x: 0, y: 0, width: 200, height: 50)
+        $0.addTarget(self, action: #selector(close), for: .touchUpInside)
+        return $0
+    }(UIButton())
 
     override func viewDidLoad() {
         super.viewDidLoad()
        
         title = "Web Socket Test"
         view.backgroundColor = .yellow
+        
+        self.view.addSubview(closeButton)
+        self.closeButton.center = view.center
         
         let session = URLSession(configuration: .default, delegate: self, delegateQueue: OperationQueue())
         guard let url = URL(string: "wss://echo.websocket.org") else { return }
@@ -32,12 +46,12 @@ final class ViewController: UIViewController, URLSessionWebSocketDelegate {
         }
     }
     
-    private func close() {
+    @objc private func close() {
         webSocket?.cancel(with: .goingAway, reason: "Demo ended".data(using: .utf8))
     }
     
     private func send() {
-        DispatchQueue.global().asyncAfter(deadline: .now() + 2) { [weak self] in
+        DispatchQueue.global().asyncAfter(deadline: .now() + 1.5) { [weak self] in
             self?.send()
             self?.webSocket?.send(.string("Send new message - \(Int.random(in: 0..<999))"), completionHandler: { error in
                 if let error = error {
@@ -76,8 +90,7 @@ final class ViewController: UIViewController, URLSessionWebSocketDelegate {
     }
     
     func urlSession(_ session: URLSession, webSocketTask: URLSessionWebSocketTask, didCloseWith closeCode: URLSessionWebSocketTask.CloseCode, reason: Data?) {
-        print("Did close connection with reason")
+        print("Did close connection with reason - \(String(describing: reason))")
     }
-
 }
 
